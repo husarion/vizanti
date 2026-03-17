5dc118c4d87b93bc6c7de2af2afca8ff9e5b28b6
kmakd <kmakd197@gmail.com>
2026-03-17 09:33:32 +0000

    revert unintentional change

--- vizanti_server/public/templates/waypoints/waypoints_script.js
+++ vizanti_server/public/templates/waypoints/waypoints_script.js
@@ -103,7 +103,10 @@ missionNodeNamebox.addEventListener("change", async () => {
 	saveSettings();
 });
 
-missionSelect.addEventListener('mousedown', updateMissionSelect);
+missionSelect.addEventListener('mousedown', () => {
+	updateMissionSelect();
+});
+
 missionSelect.addEventListener('change', () => {
 	const selectedMission = missionSelect.value;
 	const missionData = JSON.parse(selectedMission);
